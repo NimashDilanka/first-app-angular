a1ef4381b7f11e7dedf18332c123158786691ada
Nimash <nimashdilanka.13@cse.mrt.ac.lk>
2019-12-28 15:24:14 +0530

    c8: data binding - property binding

--- src/app/field/field.component.ts
+++ src/app/field/field.component.ts
@@ -5,12 +5,23 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './field.component.html',
   styleUrls: ['./field.component.css']
 })
-export class FieldComponent {
+export class FieldComponent implements OnInit {
   fieldName = 'userName';
   age = 23;
   status = 'COMPLETED';
+  setDisable = false;
 
   getStatus() {
     return this.status;
+  }
+
+  constructor() {
+    setTimeout(() => {
+        this.setDisable = true;
+      },
+      2000);
+  }
+
+  ngOnInit(): void {
   }
 }
