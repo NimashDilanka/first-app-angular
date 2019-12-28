8ee96f2d14db8f7cd9f843ed0962a3ee0a446511
Nimash <nimashdilanka.13@cse.mrt.ac.lk>
2019-12-28 12:59:25 +0530

    c5: defining seleector as HTML element

--- src/app/field/field.component.ts
+++ src/app/field/field.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-field',
+  selector: '[app-field]',
   templateUrl: './field.component.html',
   styleUrls: ['./field.component.css']
 })
