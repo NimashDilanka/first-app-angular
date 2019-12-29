e62d02012557b2efbdcece18c2983352ff76f550
Nimash <nimashdilanka.13@cse.mrt.ac.lk>
2019-12-29 07:02:47 +0530

    c12: data binding - example

--- src/app/field/field.component.ts
+++ src/app/field/field.component.ts
@@ -6,32 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./field.component.css']
 })
 export class FieldComponent implements OnInit {
-  fieldName = 'userName';
-  age = 23;
-  status = 'COMPLETED';
-  enableClick = false;
-  saveMsg = 'not yet saved';
   personName = '';
-
-  getStatus() {
-    return this.status;
-  }
-
-  onSaveClick() {
-    this.saveMsg = 'Application Saved';
-  }
-
-  constructor() {
-    setTimeout(() => {
-        this.enableClick = true;
-      },
-      2000);
-  }
-
-  onUpdateName(event: Event) {
-    console.log(event);
-    this.personName = (event.target as HTMLInputElement).value;
-  }
 
   ngOnInit(): void {
   }
